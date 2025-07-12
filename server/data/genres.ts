e7e199b549cb122599b743e05025f9b9e2278510
Viktoria Esterkin <sinichenko.vikki@gmail.com>
2025-07-12 10:55:46 +0300

    feat: add additional mock data (genres)

---
+++ server/data/genres.ts
@@ -0,0 +1,9 @@
+import type { Genre } from "../types/Genre";
+
+export const mockGenres: Genre[] = [
+    { id: "1", name: "Fiction" },
+    { id: "2", name: "Literary Fiction" },
+    { id: "3", name: "Non-fiction"},
+    { id: "4", name: "Classics" },
+    { id: "5", name: "Philosophy"}
+];
