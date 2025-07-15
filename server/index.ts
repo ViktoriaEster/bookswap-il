06306bc54167fe1fa5009a4f053b64182019f29f
Viktoria Esterkin <sinichenko.vikki@gmail.com>
2025-07-15 11:22:22 +0300

    feat: add new routes to index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,12 @@
 import express = require("express");
 import cors = require("cors");
 import booksRouter from "./routes/books";
+import usersRouter from "./routes/users";
+import authorsRouter from "./routes/authors";
+import citiesRouter from "./routes/cities";
+import commentsRouter from "./routes/comments";
+import genresRouter from "./routes/genres";
+import languagesRouter from "./routes/languages";
 
 const app = express();
 const PORT = 3001;
@@ -9,6 +15,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/books", booksRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/authors", authorsRouter);
+app.use("/api/cities", citiesRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/genres", genresRouter);
+app.use("/api/languages", languagesRouter);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}`);
